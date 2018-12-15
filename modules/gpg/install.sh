4954e56b12035e72f01bfbb69e8073e770b4cbdf
Jiří Málek <jiri.malek@gmail.com>
2018-12-16 00:58:18 +0100

    Adds gpg module.

---
+++ modules/gpg/install.sh
@@ -0,0 +1,13 @@
+echo "> installing gpg module"
+
+if [[ -d ~/.gnupg ]]; then
+    link_file "${DOTFILES_MODULES_ROOT}/gpg/gpg.conf" "${HOME}/.gnupg/gpg.conf"
+    # TODO: first check, if pinentry-mac is installed
+    link_file "${DOTFILES_MODULES_ROOT}/gpg/gpg-agent.conf" "${HOME}/.gnupg/gpg-agent.conf"
+    # TODO: restart gpg-agent
+else
+    info "no action required"
+    return
+fi
+
+success "installation complete"
