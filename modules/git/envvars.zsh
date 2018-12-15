33ad29c6ae6664a0aba0e0a04f7cd0ecd10f15e8
Jiří Málek <jiri.malek@gmail.com>
2018-12-16 00:57:20 +0100

    Adds ID of my personal gpg pubkey.

--- modules/git/envvars.zsh
+++ modules/git/envvars.zsh
@@ -2,7 +2,7 @@
 #   2.  ENVIRONMENT CONFIGURATION
 #   -------------------------------
 
-    GIT_IDENTITY_LIST=("Jiří Málek:jiri.malek@gmail.com")
+    GIT_IDENTITY_LIST=("Jiří Málek:jiri.malek@gmail.com:6342B8048A48DF7458FB12ADC6AEBEBE3B133A81")
 
     export GIT_CONFIG_NOSYSTEM=1                # ignores /etc/gitconfig
     export GIT_IDENTITY_LIST
