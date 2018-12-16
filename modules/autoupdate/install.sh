8b912d267ae70f92bf5cc470bc6973ce34532f30
Jiří Málek <jiri.malek@gmail.com>
2018-12-16 23:12:58 +0100

    Renames modules/bin/dot_update.sh -> modules/bin/dot_update.

--- modules/autoupdate/install.sh
+++ modules/autoupdate/install.sh
@@ -1,8 +1,8 @@
 echo "› installing autoupdate module"
 
 (
-    crontab -l | grep -v "dot_update.sh"
-    echo "0 */2 * * * '${DOTFILES_MODULES_ROOT}/bin/dot_update.sh' > '${DOTFILES_ROOT}/dot_update.log' 2>&1"
+    crontab -l | grep -v "dot_update"
+    echo "0 */2 * * * '${DOTFILES_MODULES_ROOT}/bin/dot_update' > '${DOTFILES_ROOT}/dot_update.log' 2>&1"
 ) | crontab -
 
 success "installation complete"
