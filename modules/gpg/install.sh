670d417e700b8bbec40d3c513d644f63f8db28fb
Jiří Málek <jiri.malek@gmail.com>
2018-12-17 14:14:26 +0100

    Creates gpg-agent.conf file for macOS and checks if needed SW is installed.

--- modules/gpg/install.sh
+++ modules/gpg/install.sh
@@ -2,8 +2,15 @@ echo "› installing gpg module"
 
 if [[ -d ~/.gnupg ]]; then
     link_file "${DOTFILES_MODULES_ROOT}/gpg/gpg.conf" "${HOME}/.gnupg/gpg.conf"
-    # TODO: first check, if pinentry-mac is installed
-    link_file "${DOTFILES_MODULES_ROOT}/gpg/gpg-agent.conf" "${HOME}/.gnupg/gpg-agent.conf"
+
+    if [[ "${DOTFILES_OS}" == "macos" ]]; then
+        if is_installed "/usr/local/bin/pinentry-mac"; then
+            link_file "${DOTFILES_MODULES_ROOT}/gpg/gpg-agent.macos.conf" "${HOME}/.gnupg/gpg-agent.conf"
+        else
+            fail "/usr/local/bin/pinentry-mac is not installed yet"
+        fi
+    fi
+
     # TODO: restart gpg-agent
 else
     info "no action required"
