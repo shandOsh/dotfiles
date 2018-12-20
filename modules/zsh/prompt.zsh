6d54abe8c61ff7898d3f54bf71d269311412136a
Jiří Málek <jiri.malek@gmail.com>
2018-12-20 13:50:21 +0100

    Adds formatting to the prompt.

--- modules/zsh/prompt.zsh
+++ modules/zsh/prompt.zsh
@@ -2,11 +2,19 @@
 #   2.  PROMPT
 #   -------------------------------
 
+    # inspired by mathiasbynens (https://github.com/mathiasbynens/dotfiles/blob/master/.bash_prompt)
     PS1=""
     PS1+="________________________________________________________________________________" # first line
     PS1+=$'\n'
     PS1+="| "
-    PS1+="%n" # username
+
+    # highlight the user name when logged in as root
+    if [[ "${UID}" -eq 0 ]]; then
+        PS1+="$(format_message --bold --color red "%n")" # username
+    else
+        PS1+="%n" # username
+    fi
+
     PS1+="@"
     PS1+="%M" # hostname
     PS1+=" "
