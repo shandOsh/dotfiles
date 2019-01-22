adc6cb22a0644ae375c302b1cd5f92652dd68eb1
Jiří Málek <jiri.malek@gmail.com>
2019-01-22 21:43:03 +0100

    Moves helpers.sh and formatting.sh from installation dir to libs dir (fixes #15).

--- libs/helpers.sh
+++ libs/helpers.sh
@@ -1,5 +1,5 @@
 # load modules
-. "${DOTFILES_INSTALLATION_ROOT}/formatting.sh"
+. "${DOTFILES_LIBS_ROOT}/formatting.sh"
 
 function success() {
     printf "\r\033[2K  [  \033[00;32mOK\033[0m  ] ${1}\n"
