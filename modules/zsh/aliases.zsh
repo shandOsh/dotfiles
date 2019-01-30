shandOsh/dotfiles

commit dc0eb4daed1ae06caa93d2ab2f1bc4df87b9d840

Unifies usage of $VAR to ${VAR}.

modules/zsh/aliases.zsh
@@ -23,7 +23,7 @@
 #   5.  FUNCTIONS
 #   -----------------------------
 
-    cd () { builtin cd "$@" && ll; }            # always list directory contents upon 'cd'
-    ccd () { builtin cd "$@" }                  # cdd: buildin cd command (used for directories with huge number of files inside)
-    mcd () { mkdir -p "$1" && cd "$1"; }        # mcd: makes new dir and jumps inside
-    mvcd () { mv "$1" "$2" && cd "$2" }         # mvcd moves directory to target and cd into it
+    cd () { builtin cd "${@}" && ll; }            # always list directory contents upon 'cd'
+    ccd () { builtin cd "${@}" }                  # cdd: buildin cd command (used for directories with huge number of files inside)
+    mcd () { mkdir -p "${1}" && cd "${1}"; }        # mcd: makes new dir and jumps inside
+    mvcd () { mv "${1}" "${2}" && cd "${2}" }         # mvcd moves directory to target and cd into it

modules/git/aliases.zsh
@@ -101,7 +101,7 @@
 
         if is_installed "gpg" && [[ "${git_user_skey}" != "" ]]; then
             gpg --list-key "${git_user_skey}" > /dev/null 2>&1
-            rc=$?
+            rc=${?}
 
             if [[ ${rc} -ne 0 ]]; then
                 echo
@@ -133,12 +133,12 @@
 
     # download gitignore for given os/ide/programming language
     function download_gitignore() {
-        if [[ $# -eq 0 ]]; then
+        if [[ ${#} -eq 0 ]]; then
             >&2 echo "No os/ide/programming language given."
             return 1
         fi
 
-        local content="$(curl -s "https://www.gitignore.io/api/$1")"
+        local content="$(curl -s "https://www.gitignore.io/api/${1}")"
 
         if [[ "${content}" =~ \#\!\!\ ERROR ]]; then
             >&2 echo "Unsupported os/ide/programming language."

installation/helpers.sh
@@ -2,19 +2,19 @@
 . "${DOTFILES_INSTALLATION_ROOT}/formatting.sh"
 
 function success() {
-    printf "\r\033[2K  [  \033[00;32mOK\033[0m  ] $1\n"
+    printf "\r\033[2K  [  \033[00;32mOK\033[0m  ] ${1}\n"
 }
 
 function fail() {
     local action
     local message
 
-    if [[ $# -eq 2 ]]; then
-        action="$1"
-        message="$2"
+    if [[ ${#} -eq 2 ]]; then
+        action="${1}"
+        message="${2}"
     else
         action=""
-        message="$1"
+        message="${1}"
     fi
 
     printf "\r\033[2K  [ \033[0;31mFAIL\033[0m ] ${message}\n"
@@ -25,15 +25,15 @@ function fail() {
 }
 
 function info() {
-    printf "\r  [ \033[00;33mINFO\033[0m ] $1\n"
+    printf "\r  [ \033[00;33mINFO\033[0m ] ${1}\n"
 }
 
 function skipped() {
-    printf "\r  [ \033[00;34mSKIP\033[0m ] $1\n"
+    printf "\r  [ \033[00;34mSKIP\033[0m ] ${1}\n"
 }
 
 function backup_file() {
-    local backup_file="$1"
+    local backup_file="${1}"
 
     if [[ ! -e "${backup_file}" ]]; then
         info "no backup needed (${backup_file} doesn't exist)"
@@ -45,8 +45,8 @@ function backup_file() {
 }
 
 function link_file() {
-    local source="$1"
-    local target="$2"
+    local source="${1}"
+    local target="${2}"
 
     if [[ "$(readlink "${target}")" == "${source}" ]]; then
         skipped "no need to link ${source}, it's already symlinked"
@@ -70,7 +70,7 @@ function os_detection() {
 }
 
 function is_installed() {
-    which "$1" > /dev/null 2>&1 && return 0
+    which "${1}" > /dev/null 2>&1 && return 0
 
     return 1
 }

installation/set-zsh.sh
@@ -28,7 +28,7 @@ case "${DOTFILES_OS}" in
         return 1
 esac
 
-rc=$?
+rc=${?}
 
 if [[ ${rc} -eq 0 ]]; then
     success "default shell set to ${DOTFILES_SHELL}"

modules/bin/git-nuke
@@ -4,12 +4,12 @@
 #
 # Nukes a branch or tag locally and on the origin remote.
 #
-# $1 - Branch/tag name.
+# ${1} - Branch/tag name.
 #
 # Examples
 #
 #   git nuke add-git-nuke
 
-git show-ref --verify refs/tags/"$1" && git tag -d "$1"
-git show-ref --verify refs/heads/"$1" && git branch -D "$1"
-git push --delete origin "$1"
+git show-ref --verify refs/tags/"${1}" && git tag -d "${1}"
+git show-ref --verify refs/heads/"${1}" && git branch -D "${1}"
+git push --delete origin "${1}"

modules/vscode/install.sh
@@ -7,12 +7,12 @@ fi
 
 case "${DOTFILES_OS}" in
     macos)
-        VSCODE_HOME="$HOME/Library/Application Support/Code"
+        VSCODE_HOME="${HOME}/Library/Application Support/Code"
         info "setting home of VS Code for macOS"
     ;;
 
     linux)
-        VSCODE_HOME="$HOME/.config/Code"
+        VSCODE_HOME="${HOME}/.config/Code"
         info "setting home of VS Code for linux"
     ;;
 

tests/test.sh
@@ -15,7 +15,7 @@ export HOME="/tmp/dotfiles.test"
 export DOTFILES_ROOT="${HOME}/.dotfiles"
 
 # if [[ -d "${HOME}" ]]; then
-#     echo "You have to manually remove fake \$HOME directory (pointing to ${HOME})."
+#     echo "You have to manually remove fake \${HOME} directory (pointing to ${HOME})."
 #     exit 1
 # fi
 

commit 63b881795c1f6f06ad38f6c6a3b08924179c739a

Fixes comment identation in modules/zsh/aliases.zsh caused with dc0eb4d.

modules/zsh/aliases.zsh
@@ -25,7 +25,7 @@
 #   5.  FUNCTIONS
 #   -----------------------------
 
-    cd () { builtin cd "${@}" && ll; }            # always list directory contents upon 'cd'
-    ccd () { builtin cd "${@}" }                  # cdd: buildin cd command (used for directories with huge number of files inside)
-    mcd () { mkdir -p "${1}" && cd "${1}"; }        # mcd: makes new dir and jumps inside
-    mvcd () { mv "${1}" "${2}" && cd "${2}" }         # mvcd moves directory to target and cd into it
+    cd () { builtin cd "${@}" && ll; }          # always list directory contents upon 'cd'
+    ccd () { builtin cd "${@}" }                # cdd: buildin cd command (used for directories with huge number of files inside)
+    mcd () { mkdir -p "${1}" && cd "${1}"; }    # mcd: makes new dir and jumps inside
+    mvcd () { mv "${1}" "${2}" && cd "${2}" }   # mvcd moves directory to target and cd into it