aef9cb3cf48fe38c29e78c65ac94944ed4a426f2
Jiří Málek <jiri.malek@gmail.com>
2019-02-02 09:21:02 +0100

    Adds install script for tmux module.

---
+++ modules/tmux/install.sh
@@ -0,0 +1,20 @@
+echo "› installing tmux module"
+
+if ! is_installed "tmux"; then
+    skipped "tmux is not installed"
+    return
+fi
+
+link_file "${DOTFILES_MODULES_ROOT}/tmux/.tmux.conf" "${HOME}/.tmux.conf"
+
+if [[ ! -d "${HOME}/.tmux" ]]; then
+    mkdir -p "${HOME}/.tmux"
+fi
+
+link_file "${DOTFILES_MODULES_ROOT}/tmux/conf" "${HOME}/.tmux/conf"
+
+# reload config
+tmux source-file ~/.tmux.conf
+success "reloading configuration"
+
+success "installation complete"
