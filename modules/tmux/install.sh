3052de8e341122487db9843c1481a7ddc25beeb7
Jiří Málek <jiri.malek@gmail.com>
2019-02-04 17:31:53 +0100

    Splits installation into 2 parts - the latter is executed only if program is installed (fixes #18).

--- modules/tmux/install.sh
+++ modules/tmux/install.sh
@@ -1,10 +1,5 @@
 echo "› installing tmux module"
 
-if ! is_installed "tmux"; then
-    skipped "tmux is not installed"
-    return
-fi
-
 link_file "${DOTFILES_MODULES_ROOT}/tmux/.tmux.conf" "${HOME}/.tmux.conf"
 
 if [[ ! -d "${HOME}/.tmux" ]]; then
@@ -14,8 +9,15 @@ fi
 
 link_file "${DOTFILES_MODULES_ROOT}/tmux/conf" "${HOME}/.tmux/conf"
 
+success "installation complete"
+
+if ! is_installed "tmux"; then
+    skipped "tmux is not installed, post-installation steps won't be run"
+    return
+fi
+
 # reload config
 tmux source-file ~/.tmux.conf
 report_status "reloading configuration"
 
-success "installation complete"
+success "post-installation steps completed"
