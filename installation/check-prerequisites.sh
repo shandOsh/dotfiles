0443bd8ea5d259c1b9cdef9527e99850f1bdbe22
Jiří Málek <jiri.malek@gmail.com>
2019-10-18 11:12:30 +0200

    Aborts installation and update if prerequisites are not met (closes #43).

---
+++ installation/check-prerequisites.sh
@@ -0,0 +1,22 @@
+prerequisites_error=0
+
+if ! is_installed "git"; then
+    prerequisites_error=1
+
+    fail "git is not installed"
+else
+    success "git is installed"
+fi
+
+if ! is_installed "zsh"; then
+    prerequisites_error=1
+
+    fail "zsh is not installed"
+else
+    success "zsh is installed"
+fi
+
+if [[ ${prerequisites_error} -ne 0 ]]; then
+    fail "prerequisites are not met, aborting"
+    exit 1
+fi
