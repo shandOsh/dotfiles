6b63e57e2d7c8c2c8b03f3d4ca6f5d63a50358dd
Jiří Málek <jiri.malek@gmail.com>
2022-03-30 09:54:58 +0200

    [fzf] Enhances preview error message.

--- modules/fzf/envvars.zsh
+++ modules/fzf/envvars.zsh
@@ -16,8 +16,10 @@
     # FZF_BIND_LIST+=( "ctrl-shift-g:preview-page-up" ) # doesn't work
     # FZF_BIND_LIST+=( "ctrl-shift-f:preview-page-down" ) # doesn't work
 
+    FZF_PREVIEW_ERROR_MSG="Can'\''t show preview, {} is a binary file."
+
     FZF_DEFAULT_OPTS=""
-    FZF_DEFAULT_OPTS+=" --preview='[[ \$(file --mime {}) =~ binary ]] && echo {} is a binary file || (bat --style=numbers,changes --color=always --theme=${FZF_BAT_THEME} {} || cat {}) 2> /dev/null | head -300'"
+    FZF_DEFAULT_OPTS+=" --preview='[[ \$(file --mime {}) =~ binary ]] && (ansi --red \"${FZF_PREVIEW_ERROR_MSG}\" || echo \"${FZF_PREVIEW_ERROR_MSG}\") || (bat --style=numbers,changes --color=always --theme=${FZF_BAT_THEME} {} || cat {}) 2> /dev/null | head -300'"
     FZF_DEFAULT_OPTS+=" --preview-window='right'"
     FZF_DEFAULT_OPTS+=" --bind='$(IFS=,; printf '%s' "${FZF_BIND_LIST[*]}")'"
 
