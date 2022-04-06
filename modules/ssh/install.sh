82039a1701d3f3c1b60e4472be28783eb90cb7be
Jiří Málek <jiri.malek@gmail.com>
2022-04-06 14:37:52 +0200

    [ssh] Replaces version checking with require_version().

--- modules/ssh/install.sh
+++ modules/ssh/install.sh
@@ -4,27 +4,13 @@ echo "› installing ssh module"
 
 # if our version of openSSH is lower than 7.3, abort the installation,
 # because the directive Include is not implemented
-ssh_version="$(ssh -V 2>&1 | egrep --only-matching '^OpenSSH_[0-9]+\.[0-9]+' | egrep --only-matching '[0-9]+\.[0-9]+')"
-ssh_version_major="$(echo "${ssh_version}" | cut -d. -f 1)"
-ssh_version_minor="$(echo "${ssh_version}" | cut -d. -f 2)"
-ssh_version_is_valid=1
+ssh_version="$(ssh -V 2>&1 | grep -E --only-matching '^OpenSSH_[0-9]+(\.[0-9]+){1,3}' | sed 's/OpenSSH_//g')"
 
-if [[ "${ssh_version}" == "" ]]; then
-    fail "cannot parse openSSH version"
-    return 1
-fi
-
-if [[ "${ssh_version_major}" -lt 7 ]]; then
-    ssh_version_is_valid=0
-elif [[ "${ssh_version_major}" -eq 7 ]] && [[ "${ssh_version_minor}" -lt 3 ]]; then
-    ssh_version_is_valid=0
-fi
-
-if [[ "${ssh_version_is_valid}" -eq 0 ]]; then
-    fail "your openSSH version ${ssh_version} is not supported, version 7.3 and newer is required"
-    return 1
-else
+if require_version "${ssh_version}" ">=" "7.3"; then
     info "your openSSH version ${ssh_version} is supported"
+else
+    fail "your openSSH version ${ssh_version} is not supported, version 7.3 or newer is required"
+    return 1
 fi
 
 if [[ ! -d "${HOME}/.ssh" ]]; then
