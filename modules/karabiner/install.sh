08efa8a3d4e4557b0bfd2a7e68192e7bc28908ac
Jiří Málek <jiri.malek@gmail.com>
2022-04-10 13:43:56 +0200

    [karabiner] Removes karabiner.json from dotfiles.

--- modules/karabiner/install.sh
+++ modules/karabiner/install.sh
@@ -12,7 +12,6 @@ if [[ ! -d "${HOME}/.config/karabiner" ]]; then
     report_status "creating directory ${HOME}/.config/karabiner"
 fi
 
-link_file "${DOTFILES_MODULES_ROOT}/karabiner/karabiner.json" "${HOME}/.config/karabiner/karabiner.json"
 link_file "${DOTFILES_MODULES_ROOT}/karabiner/assets/complex_modifications/win-cz.hp-elitebook-840-g3.json" "${HOME}/.config/karabiner/assets/complex_modifications/win-cz.hp-elitebook-840-g3.json"
 
 success "installation complete"
