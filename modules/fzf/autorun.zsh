b940fd25ef1080e4c5d8b8435a7d893b061205dc
Jiří Málek <jiri.malek@gmail.com>
2022-06-14 09:50:34 +0200

    [fzf] Autoloads OS specific shell completition and key bindings.

--- modules/fzf/autorun.zsh
+++ modules/fzf/autorun.zsh
@@ -6,4 +6,13 @@
         return
     fi
 
-    [[ -f ~/.fzf.zsh ]] && source ~/.fzf.zsh
+    case "${DOTFILES_OS}" in
+        "${DOTFILES_OS_MACOS}")
+            [[ -f ~/.fzf.zsh ]] && source ~/.fzf.zsh
+        ;;
+
+        "${DOTFILES_OS_LINUX}")
+            [[ -f /usr/share/zsh/site-functions/fzf ]] && source /usr/share/zsh/site-functions/fzf
+            [[ -f /usr/share/fzf/shell/key-bindings.zsh ]] && source /usr/share/fzf/shell/key-bindings.zsh
+        ;;
+    esac
